9603281b0fba447f14fb3ce2114a4a9a5e67b38d
Chengju Liu <liucjkv@gmail.com>
2023-11-21 00:34:31 +0800

    Created tests for Scanner and Token.

---
+++ src/main/java/hk/edu/polyu/comp/comp3211/group6/Controller/ScannerError.java
@@ -0,0 +1,2 @@
+package hk.edu.polyu.comp.comp3211.group6.Controller;public class ScannerError {
+}
---
+++ src/test/java/hk/edu/polyu/comp/comp3211/group6/Controller/ScannerTest.java
@@ -0,0 +1,6 @@
+package hk.edu.polyu.comp.comp3211.group6.Controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ScannerTest {
+  
+}
